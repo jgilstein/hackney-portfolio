81c35c266496b785d5a051b5a317e420e807cb6b
jgilstein <jgilstein@icloud.com>
2025-07-28 19:54:34 -0400

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: "sans-serif", padding: "2rem" }}>
+      <h1>Hackney Portfolio Website</h1>
+      <p>This is the live version of the Hackney internship project.</p>
+      <ul>
+        <li><a href="/Hackney_One_Pager_Public.pdf" target="_blank">One Pager PDF</a></li>
+        <li><a href="/Hackney_Portfolio_SlideDeck.pdf" target="_blank">Slide Deck</a></li>
+        <li><a href="/Hackney_Tableau_Snip_1.png" target="_blank">Tableau Snip 1</a></li>
+        <li><a href="/Hackney_Tableau_Snip_3.png" target="_blank">Tableau Snip 3</a></li>
+        <li><a href="/Hackney_Tableau_Snip_4.png" target="_blank">Tableau Snip 4</a></li>
+        <li><a href="/Hackney_Tableau_Snip_6.png" target="_blank">Tableau Snip 6</a></li>
+      </ul>
+    </main>
+  );
+}
