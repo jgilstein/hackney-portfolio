ea8ee0aa18558fec1a3ed962292f3143b1ded5c9
jgilstein <jgilstein@icloud.com>
2025-07-28 20:25:42 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <h1>Hackney Portfolio Website</h1>
       <p>This is the live version of the Hackney internship project.</p>
       <ul>
-        <li><a href="/resume.pdf" target="_blank">Resume PDF</a></li>
+        <li><a href="/resume '25.pdf" target="_blank">Resume PDF</a></li>
         <li><a href="/hackney-internship-writeup.pdf" target="_blank">Internship Writeup</a></li>
         <li><a href="/hackney-tableau.twbx" target="_blank">Tableau Workbook</a></li>
       </ul>
