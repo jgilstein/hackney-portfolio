ab615597fa4f476a15b259832eae69f64107a548
jgilstein <jgilstein@icloud.com>
2025-07-28 20:11:27 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,13 @@
 export default function Home() {
   return (
-    <main style={{ fontFamily: "sans-serif", padding: "2rem" }}>
+    <div>
       <h1>Hackney Portfolio Website</h1>
       <p>This is the live version of the Hackney internship project.</p>
       <ul>
-        <li><a href="/Hackney_One_Pager_Public.pdf" target="_blank">One Pager PDF</a></li>
-        <li><a href="/Hackney_Portfolio_SlideDeck.pdf" target="_blank">Slide Deck</a></li>
-        <li><a href="/Hackney_Tableau_Snip_1.png" target="_blank">Tableau Snip 1</a></li>
-        <li><a href="/Hackney_Tableau_Snip_3.png" target="_blank">Tableau Snip 3</a></li>
-        <li><a href="/Hackney_Tableau_Snip_4.png" target="_blank">Tableau Snip 4</a></li>
-        <li><a href="/Hackney_Tableau_Snip_6.png" target="_blank">Tableau Snip 6</a></li>
+        <li><a href="/resume.pdf" target="_blank">Resume PDF</a></li>
+        <li><a href="/hackney-internship-writeup.pdf" target="_blank">Internship Writeup</a></li>
+        <li><a href="/hackney-tableau.twbx" target="_blank">Tableau Workbook</a></li>
       </ul>
-    </main>
+    </div>
   );
 }
